46a815797e63a861c167edcda8cb40fa6cc21d26
Samuel-Alemayehu <samuel@kemercode.com>
2022-08-28 13:23:19 +0300

    i have added comment on the app

--- src/components/Detail/appreview.js
+++ src/components/Detail/appreview.js
@@ -1,23 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./output.css";
 import StarRating from "./starRate";
-import { collection, getDocs } from "@firebase/firestore";
+import { collection, getDocs, addDoc } from "@firebase/firestore";
 import { db } from "../FireBase/firebase-config";
 import { useParams } from "react-router-dom";
+
 const Appreview = () => {
   const [reviewform, showreviewform] = useState(false);
   const [comments, setComments] = useState([]);
+  const [addComment, setAddComment] = useState("");
+  const [users, setUsers] = useState([]);
+  const commentHandler = (event) => {
+    setAddComment(event.target.value);
+  };
   const params = useParams();
   const AppDetailCollection = collection(db, "Comments");
+  const UserCollection = collection(db, "Users");
   useEffect(() => {
     const getAppDetail = async () => {
       const data = await getDocs(AppDetailCollection);
       setComments(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      const userdata = await getDocs(UserCollection);
+      setUsers(userdata.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getAppDetail();
-  }, []);
+  }, [reviewform]);
 
   const appComments = comments.filter((data) => data.Id === params.list);
+  const AddReview = async () => {
+    showreviewform((prev) => !prev);
+    let use = users;
+    use = users.find((data) => data.email === "s122@gmail.com");
+    addDoc(AppDetailCollection, {
+      Id: params.list,
+      Name: use.firstName,
+      LastName: use.lastName,
+      Comment: addComment,
+      Rating: 2,
+    });
+  };
   return (
     <div>
       <div>
@@ -37,10 +58,11 @@ const Appreview = () => {
               type="text"
               id="large-input"
               class="ml-8 mt-4 block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={commentHandler}
             />
 
             <button
-              onClick={() => showreviewform(!reviewform)}
+              onClick={AddReview}
               class=" ml-4 bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded"
             >
               Save Review
