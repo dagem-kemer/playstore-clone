230da49fbad603f0a9f5c236823959fa9d18d5e4
Dagem Tadesse <dagem.seyfu@gmail.com>
2022-08-29 15:43:17 +0300

    scroll behaviour update

--- src/components/List/ListWrapper.js
+++ src/components/List/ListWrapper.js
@@ -4,11 +4,10 @@ import ChevronRightIcon from "../icons/ChevronRightIcon";
 
 export default function ListWrapper(props) {
   const [showNextButtons, setShowNextButtons] = useState(false);
-  const [scrollOffset, setScrollOffset] = useState(0);
+  const [scrollPos, setScrollPos] = useState(0);
   const divRef = useRef();
 
   const scrollRight = () => {
-    console.log(divRef.current.scrollLeft, divRef.current.clientWidth);
     divRef.current.scrollLeft += divRef.current.clientWidth;
   };
 
@@ -17,14 +16,21 @@ export default function ListWrapper(props) {
   };
 
   return (
-    <div className="relative">
+    <div
+      className="relative"
+      
+    >
       <div
         className="flex scroll-smooth overflow-x-auto snap-x snap-mandatory no-scrollbar w-full"
         onMouseEnter={() => setShowNextButtons(true)}
         onMouseLeave={() => setShowNextButtons(false)}
         ref={divRef}
+        onScroll={() => {
+          setScrollPos(divRef?.current.scrollLeft);
+          console.log(divRef.current.scrollLeft);
+        }}
       >
-        {showNextButtons && !!scrollOffset && (
+        {showNextButtons && !!scrollPos && (
           <div className="absolute top-[calc(50%-56px)] -left-3">
             <button
               className="bg-slate-50  rounded-full w-[56px] h-[56px] flex items-center justify-center shadow-lg"
