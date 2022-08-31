bdf5272b93092b9b7f53bda6cd78db19206da05d
Samuel-Alemayehu <samuel@kemercode.com>
2022-08-31 18:32:12 +0300

    fixed some bugs

--- src/components/List/AppList.js
+++ src/components/List/AppList.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ListItem from "./ListItem";
 import ListWrapper from "./ListWrapper";
-
+import { Link } from "react-router-dom";
 const AppList = ({ apps }) => {
   let listCols = [];
   let col = [];
 
   for (let app of apps) {
-    // col.push(app);
     col.push({ ...app, index: apps.indexOf(app) + 1 });
-    // console.log();
     if (col.length === 3) {
       listCols.push(col);
       col = [];
@@ -18,16 +16,19 @@ const AppList = ({ apps }) => {
 
   return (
     <ListWrapper>
-      {listCols.map((col) => (
-        <div className="basis-[33.333%] shrink-0 snap-start">
+      {listCols.map((col, index) => (
+        <div key={index} className="basis-[33.333%] shrink-0 snap-start">
           {col.map((row) => (
-            <ListItem
-              index={row.index}
-              key={row.Id}
-              image={row.ImageSrc}
-              rating={row.Rating}
-              name={row.Name}
-            />
+            <Link to={row.id}>
+              <ListItem
+                index={row.index}
+                key={row.Id}
+                image={row.ImageSrc}
+                rating={row.Rating}
+                name={row.Name}
+                type={row.Type}
+              />
+            </Link>
           ))}
         </div>
       ))}
--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,12 +1,15 @@
 import AppItem from "./AppItem";
 
-const ListItem = ({ image, index, name, rating }) => {
+const ListItem = ({ image, index, name, rating, key, type }) => {
   return (
-    <div className="flex hover:bg-black hover:bg-opacity-5 rounded-lg ml-4">
+    <div
+      key={key}
+      className="flex hover:bg-black hover:bg-opacity-5 rounded-lg ml-4"
+    >
       <section className="self-center text-darkGrey mr-2 basis-8 flex justify-center">
         <div>{index}</div>
       </section>
-      <AppItem image={image} name={name} rating={rating} />
+      <AppItem image={image} name={name} rating={rating} type={type} />
     </div>
   );
 };
